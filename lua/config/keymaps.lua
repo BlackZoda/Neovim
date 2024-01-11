fc59c687ecd6ee907cba38163bc2870b8ff13147
Ivar Abusdal <ivar.abusdal@designerkontoret.no>
2024-01-11 23:51:34 +0100

    set up osaka-solarized color theme

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,61 @@
 -- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lu
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+vim.g.mapleader = " "
+keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- shift selected lines
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- yank until end line (but no new line)
+keymap.set("n", "Y", "yg$")
+
+-- keep cursorin place when joining lines
+keymap.set("n", "J", "mzJ`z")
+
+-- cursor stays in place during half page jumping
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "<C-d>", "<C-d>zz")
+
+--vim cursor stay in place during search
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+-- keep the copy reister intact when pasting
+keymap.set("x", "<leader>p", [["_dP]])
+
+-- copy to system clip board
+keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+keymap.set("n", "<leader>Y", [["+Y]])
+
+-- cut to system clip board
+keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+keymap.set("n", "Q", "<nop>")
+keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- find/replace current word
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- make executable
+keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- go to packer file
+keymap.set("n", "<leader>vpp", "<cmd>e /Users/Abusdal/.config/nvim/lua/blackzoda/packer.lua<CR>")
+
+keymap.set("n", "<leader><leader>", function()
+  vim.cmd("so")
+end)
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
